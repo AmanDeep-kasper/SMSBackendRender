deb3761bc763770cebe438eacdd98196b7507a22
AmanDeep-kasper <amandeepempkasper@gmail.com>
2026-06-12 13:39:33 +0530

    fix render import case

--- routes/SchoolRoute.js
+++ routes/SchoolRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerSchool } from "../controllers/schoolController.js";
+import { registerSchool } from "../controllers/SchoolController.js";
 
 const router = express.Router();
 
